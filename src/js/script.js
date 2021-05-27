b1efa2d698f36a470375051c85f03b9958cc3b85
NataPlato <kirha76@gmail.com>
2021-05-27 14:19:10 +0300

    mobile adaptation products page

--- src/js/script.js
+++ src/js/script.js
@@ -16,20 +16,19 @@ const swiperHeader = new Swiper(".swiper-header", {
 });
 
 const swiperShop = new Swiper(".swiper-shop", {
-  slidesPerView: 4.5,
   navigation: {
     nextEl: ".swiper-shop__next",
     prevEl: ".swiper-shop__prev",
   },
   breakpoints: {
     768: {
-      slidesPerView: 3.5,
+      slidesPerView: 3,
     },
     780: {
-      slidesPerView: 4,
+      slidesPerView: 3.5,
     },
     970: {
-      slidesPerView: 4.5,
+      slidesPerView: 4,
     },
     1200: {
       slidesPerView: 5,
@@ -56,12 +55,27 @@ const swiperProducts = new Swiper(".swiper-products", {
 });
 
 const paddingSlider = document.querySelector(".filter__line");
+if (paddingSlider) {
+  noUiSlider.create(paddingSlider, {
+    start: [0, 200],
+    connect: [false, true, false],
+    range: {
+      min: 0,
+      max: 200,
+    },
+    format: wNumb({
+      decimals: 0,
+      thousand: ".",
+      suffix: " $",
+    }),
+  });
 
-noUiSlider.create(paddingSlider, {
-  start: [0, 150],
-  connect: [false, true, false],
-  range: {
-    min: 0,
-    max: 100,
-  },
-});
+  const skipValues = [
+    document.getElementById("skip-value-lower"),
+    document.getElementById("skip-value-upper"),
+  ];
+
+  paddingSlider.noUiSlider.on("update", function (values, handle) {
+    skipValues[handle].innerHTML = values[handle];
+  });
+}
