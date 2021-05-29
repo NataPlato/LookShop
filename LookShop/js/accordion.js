429c8dfe534da1775064637f51c707afb25b9f1b
NataPlato <kirha76@gmail.com>
2021-05-29 14:49:31 +0300

    finished

--- LookShop/js/accordion.js
+++ LookShop/js/accordion.js
@@ -1,6 +1,7 @@
 "use strict";
 document.addEventListener("DOMContentLoaded", () => {
-  const accordionBtn = document.querySelectorAll(".categories__block_title");
+  const accordionBtn = document.querySelectorAll(".categories__block_title"),
+      accordionTitle = document.querySelectorAll(".accordion__block_title");
 
   accordionBtn.forEach(e => {
     e.addEventListener("click", () => {
@@ -9,6 +10,15 @@ document.addEventListener("DOMContentLoaded", () => {
           btn.classList.remove("categories__block_title-active");
       });
       e.classList.toggle("categories__block_title-active");
+    });
+  });
+  accordionTitle.forEach(e => {
+    e.addEventListener("click", () => {
+      accordionTitle.forEach(btn => {
+        if (btn.classList.contains("accordion__block_title-active") && btn !== e)
+          btn.classList.remove("accordion__block_title-active");
+      });
+      e.classList.toggle("accordion__block_title-active");
     });
   });
 });
